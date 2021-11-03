9eb787e5d965a57cdb55f737f37dc8eb9446a153
Emiria <Emiriachan@users.noreply.github.com>
2021-11-03 22:01:51 +0800

    completed primary album.py

--- album.py
+++ album.py
@@ -1,5 +1,12 @@
-"""..."""
-
-
 class Album:
-    pass
+    def __init__(self, title, artist, year, is_completed):
+        self.title = title
+        self.artist = artist
+        self.year = year
+        self.is_completed = is_completed
+
+    def __str__(self):
+        if self.is_completed:
+            return "completed"
+        else:
+            return "required"
--- albumcollection.py
+++ albumcollection.py
@@ -1,5 +1,25 @@
-"""..."""
+import csv
 
 
 class AlbumCollection:
-    pass
+    def __init__(self):
+        self.list_of_albums = [[]]
+        self.add_album = []
+
+    def load_albums(self):
+        with open("albums.csv", "r") as albums:
+            reader = csv.reader(albums)
+            self.list_of_albums = list(reader)
+
+    def save_albums(self):
+        with open("albums.csv", "w") as albums:
+            writer = csv.writer(albums)
+            writer.writerows(self.list_of_albums)
+
+    def add_album(self):
+        with open("albums.csv", "a") as albums:
+            writer = csv.writer(albums)
+            writer.writerow(self.add_album)
+
+    def sort(self, ):
+
