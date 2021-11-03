fb5625ce961c1d0c5907efe375b84ed261fe32f2
Emiria <Emiriachan@users.noreply.github.com>
2021-11-03 20:51:49 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Name:
+Date:
+Brief Project Description:
+"""
+# Create your main program in this file, using the AlbumTrackerApp class
+
+from kivy.app import App
+
+
+class AlbumTrackerApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    AlbumTrackerApp().run()
